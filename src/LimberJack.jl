36c46421c6c773b5865ce6098c0ec7b879f29daf
Andrina Nicola <aaxnicola@gmail.com>
2021-09-22 16:14:58 -0400

    First go at halofit.

--- src/LimberJack.jl
+++ src/LimberJack.jl
@@ -1,16 +1,20 @@
 module LimberJack
 
 export Cosmology
-export Ez, Hmpc, comoving_radial_distance, power_spectrum, growth_factor
+export Ez, Hmpc, comoving_radial_distance, power_spectrum, growth_factor, omega_x
 export NumberCountsTracer
 export angularCℓ
+export PKnonlin
 
 using Interpolations
 using QuadGK
 using OrdinaryDiffEq
+using Trapz
+using Roots
 
 include("core.jl")
 include("tracers.jl")
 include("spectra.jl")
+include("halofit.jl")
 
 end
