2caf3109767af21481fe1300970894b3217c648e
Maquiner <jaimerz011235813@gmail.com>
2021-11-18 15:22:25 +0000

    Clustering working

--- src/core.jl
+++ src/core.jl
@@ -216,39 +216,3 @@ end
 
 # Make the Cl integral in K
 # Compare to David's tracers.jl Number_counts
-
-struct NumberCountsTracer{T<:Real}
-    wint::AbstractInterpolation{T, 1}
-    bias::T
-    wnorm::T
-end
-
-NumberCountsTracer(z, wz, bias) = begin
-    wint = LinearInterpolation(z, wz, extrapolation_bc=0)
-    area = quadgk(wint, z[1], z[end], rtol=1E-5)[1]
-    NumberCountsTracer(wint, bias, 1.0/area)
-end
-
-function Cℓintegrand(cosmo::Cosmology,
-                     t1::NumberCountsTracer,
-                     t2::NumberCountsTracer,
-                     logk::Float64, ℓ::Int64)
-    k = exp(logk)
-    chi = (ℓ+0.5)/k
-    if chi > cosmo.chi_max
-        return 0
-    end
-    z = cosmo.z_of_chi(chi)
-    hz = Hmpc(cosmo, z)
-    w1 = t1.wint(z)*t1.wnorm*hz*t1.bias
-    w2 = t2.wint(z)*t2.wnorm*hz*t1.bias
-    pk = power_spectrum(cosmo, k, z)
-    k*w1*w2*pk
-end
-
-function angularCℓ(cosmo, t1, t2, ℓ)
-    quadgk(lk -> Cℓintegrand(cosmo, t1, t2, lk, ℓ),
-           log(10^-4), log(10^2), rtol=1E-5)[1]/(ℓ+0.5)
-end
-
-        
