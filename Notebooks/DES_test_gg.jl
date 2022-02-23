9e1cdb1f19f8f4bbe1ca5d785cb6d06f9be5f088
jaimerzp <jaimerz011235813@gmail.com>
2022-02-23 09:48:05 +0000

    files from glam

--- Notebooks/DES_test_gg.jl
+++ Notebooks/DES_test_gg.jl
@@ -8,15 +8,17 @@ using FITSIO
 using Plots
 using LinearAlgebra
 
-des_data = npzread("/home/jaime/PhD/LimberJack.jl/data/cl_DESgc__2_DESgc__2.npz")["cl"][1:39]
-des_ell = npzread("/home/jaime/PhD/LimberJack.jl/data/cl_DESgc__2_DESgc__2.npz")["ell"]
+des_data = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cl_DESgc__2_DESwl__3.npz")["cl"]
+des_data = transpose(des_data)[1:39]
+des_ell = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cl_DESgc__2_DESwl__3.npz")["ell"]
 des_ell = [Int(floor(l)) for l in des_ell]
-des_cov = npzread("/home/jaime/PhD/LimberJack.jl/data/cov_DESgc__2_DESgc__2_DESgc__2_DESgc__2.npz")["cov"]
+des_cov = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cov_DESgc__2_DESwl__3_DESgc__2_DESwl__3.npz")["cov"]
+des_cov = des_cov[1:39, 1:39]
 des_cov = Symmetric(Hermitian(des_cov))
 des_err = (view(des_cov, diagind(des_cov)))[1:39].^0.5
-des_nzs = FITS("/home/jaime/PhD/LimberJack.jl/data/y1_redshift_distributions_v1.fits")
+des_nzs = FITS("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/y1_redshift_distributions_v1.fits")
 des_nz = read(des_nzs["nz_source_mcal"], "BIN2")
-des_zs = read(des_nzs["nz_source_mcal"], "Z_MID");
+des_zs = read(des_nzs["nz_source_mcal"], "Z_MID")
 
 @model function model(data)
     Ωm ~ Uniform(0.2, 0.3)
--- src/halofit.jl
+++ src/halofit.jl
@@ -28,11 +28,12 @@ function get_PKnonlin(cosmo::CosmoPar, z, k, PkLz0, Dzs)
 
     Dz2s = Dzs .^ 2
     # OPT: hard-coded range and number of points
-    lR0 = log(0.1)
+    lR0 = log(0.01)
     lR1 = log(10.0)
-    lRs = range(lR0, stop=lR1, length=64)
+    lRs = range(lR0, stop=lR1, length=100)
     lσ2s = log.([get_σ2(logk, k, PkLz0, exp(lR), 0) for lR in lRs])
-    lσ2i = CubicSplineInterpolation(lRs, lσ2s)
+    # When s8<0.6 lσ2i interpolation fails --> Extrapolation needed
+    lσ2i = CubicSplineInterpolation(lRs, lσ2s) #, extrapolation_bc=Line())
     rsigs = [get_rsigma(lσ2i, Dz2s[i], lR0, lR1) for i in range(1, stop=nz)]
 
     onederiv_ints = [get_σ2(logk, k, PkLz0, rsigs[i], 2) * Dz2s[i] for i in range(1, stop=nz)]
