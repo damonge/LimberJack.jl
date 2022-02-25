3691cc63f9b9dc953f33e024d9965767b70bf29a
jaimerzp <jaimerz011235813@gmail.com>
2022-02-25 16:30:26 +0000

    Data.jl

--- src/LimberJack.jl
+++ src/LimberJack.jl
@@ -4,6 +4,7 @@ export CosmoPar, Cosmology
 export Ez, Hmpc, comoving_radial_distance, growth_factor
 export NumberCountsTracer, WeakLensingTracer, CMBLensingTracer, get_Fℓ
 export angularCℓ, lin_Pk, nonlin_Pk
+export Data, Nz
 
 using Interpolations, QuadGK, OrdinaryDiffEq, Trapz, ForwardDiff, LinearAlgebra
 
@@ -11,5 +12,6 @@ include("core.jl")
 include("tracers.jl")
 include("spectra.jl")
 include("halofit.jl")
+include("data.jl")
 
 end
