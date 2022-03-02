b530cb8f550e0407e1fa0972a14aa0b058f30776
jaimerzp <jaimerz011235813@gmail.com>
2022-03-02 16:53:19 +0000

    get_data_vector

--- src/LimberJack.jl
+++ src/LimberJack.jl
@@ -4,7 +4,7 @@ export CosmoPar, Cosmology
 export Ez, Hmpc, comoving_radial_distance, growth_factor
 export NumberCountsTracer, WeakLensingTracer, CMBLensingTracer, get_Fℓ
 export angularCℓ, lin_Pk, nonlin_Pk
-export Data, Nz
+export Data, Nz, get_data_vector, get_tot_cov
 
 using Interpolations, QuadGK, OrdinaryDiffEq, Trapz, ForwardDiff, LinearAlgebra, NPZ, FITSIO
 
--- src/data.jl
+++ src/data.jl
@@ -11,6 +11,7 @@ struct Data <: Data_typ
     cov::Matrix
     nz1::Nz_typ
     nz2::Nz_typ
+    path::String
 end
 
 function Data(tracer1, tracer2, bin1, bin2; path="LimberJack.jl/data/")
@@ -31,7 +32,7 @@ function Data(tracer1, tracer2, bin1, bin2; path="LimberJack.jl/data/")
     cov = Symmetric(Hermitian(cov))
     nz1 = Nz(bin1; path=path)
     nz2 = Nz(bin2; path=path)
-    Data(tracer1, tracer2, bin1, bin2, cl, ell, cov, nz1, nz2)
+    Data(tracer1, tracer2, bin1, bin2, cl, ell, cov, nz1, nz2, path)
 end
 
 struct Nz <: Nz_typ
@@ -46,4 +47,27 @@ function Nz(bin_number; path="LimberJack.jl/data/")
     nz = read(nzs["nz_source_mcal"], bin_name)
     zs = read(nzs["nz_source_mcal"], "Z_MID")
     Nz(nz, zs)
+end
+
+function get_data_vector(datas)
+    return vcat([data.cl for data in datas]...)
+end
+
+function get_tot_cov(datas; path="LimberJack.jl/data/")
+    cls_meta = [string(data.tracer1, "__", data.bin1, "_", 
+                       data.tracer2, "__", data.bin2) 
+                for data in datas]
+    cov_meta = [string(cl_i, "_", cl_j) for cl_i in cls_meta for cl_j in cls_meta]
+    covs_fnames = [string(path, "cov_", cov_fname, ".npz") for cov_fname in cov_meta]
+    cov_tot = []
+    for cov_fname in covs_fnames
+        if isfile(cov_fname)
+            cov = npzread(cov_fname)["cov"]
+            cov = cov[1:39, 1:39]
+            push!(cov_tot, cov)
+        else 
+            push!(cov_tot, zeros(39,39))
+        end
+    end
+    return cov_tot = reshape(vcat(cov_tot...), (78,78))    
 end
