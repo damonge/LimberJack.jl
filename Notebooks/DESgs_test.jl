219d49a230d784239808f903cc4d2c5756c9d093
jaimerzp <jaimerz011235813@gmail.com>
2022-03-08 13:11:41 +0000

    changing naming conventions

---
+++ Notebooks/DESgs_test.jl
@@ -0,0 +1,40 @@
+using Turing
+using LimberJack
+using CSV
+using NPZ
+using FITSIO
+
+des_data = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cl_DESgc__2_DESwl__3.npz")["cl"]
+des_data = transpose(des_data)[1:39]
+des_ell = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cl_DESgc__2_DESwl__3.npz")["ell"]
+des_ell = [Int(floor(l)) for l in des_ell]
+des_cov = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cov_DESgc__2_DESwl__3_DESgc__2_DESwl__3.npz")["cov"]
+des_cov = des_cov[1:39, 1:39]
+des_cov = Symmetric(Hermitian(des_cov))
+des_err = (view(des_cov, diagind(des_cov)))[1:39].^0.5
+des_nzs = FITS("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/y1_redshift_distributions_v1.fits")
+des_nz = read(des_nzs["nz_source_mcal"], "BIN2")
+des_zs = read(des_nzs["nz_source_mcal"], "Z_MID")
+
+@model function model(data)
+    Ωm ~ Uniform(0.2, 0.3)
+    h ~ Uniform(0.6, 0.8)
+    s8 ~ Uniform(0.7, 1.0)
+    cosmology = LimberJack.Cosmology(Ωm, 0.05, h, 0.96, s8,
+                                     tk_mode="EisHu",
+                                     Pk_mode="Halofit")
+    tg = NumberCountsTracer(cosmology, des_zs, des_nz, 2.)
+    ts = WeakLensingTracer(cosmology, des_zs, des_nz)
+    predictions = [angularCℓ(cosmology, tg, ts, ℓ) for ℓ in des_ell]
+    data ~ MvNormal(predictions, des_cov)
+end;
+
+iterations = 100
+step_size = 0.05
+samples_per_step = 10
+
+# Start sampling.
+chain = sample(model(data), HMC(step_size, samples_per_step), iterations, progress=true)
+
+
+CSV.write("Cl_DESgs_test_chain.csv", chain)
---
+++ Notebooks/Test_parallelization.jl
@@ -0,0 +1,74 @@
+using Turing
+using Random
+using ForwardDiff
+using LimberJack
+using CSV
+using NPZ
+using FITSIO
+
+# Add four processes to use for sampling.
+using ClusterManager
+#add 20 processes on the long queue
+cores = 4
+ClusterManagers.addprocs_lsf(cores; bsub_flags = `-q berg`)
+#loading Turing on the processes
+@everywhere using Turing, LimberJack
+
+ell = npzread("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/cl_DESgc__2_DESwl__3.npz")["ell"]
+ell = [Int(floor(l)) for l in ell]
+nzs = FITS("/mnt/zfsusers/jaimerz/PhD/LimberJack.jl/data/y1_redshift_distributions_v1.fits")
+nz = read(nzs["nz_source_mcal"], "BIN2")
+zs = read(nzs["nz_source_mcal"], "Z_MID")
+
+true_cosmology = LimberJack.Cosmology(0.25, 0.05, 0.67, 0.96, 0.81,
+                                      tk_mode="EisHu", Pk_mode="Halofit");
+tg = NumberCountsTracer(true_cosmology, zs, nz, 2.)
+ts = WeakLensingTracer(true_cosmology, zs, nz)
+true_data = [angularCℓ(true_cosmology, tg, ts, ℓ) for ℓ in ell]
+data = true_data #+ 0.1 * true_data .* rand(length(true_data)) 
+
+@model function model(data)
+    Ωm ~ Uniform(0.1, 0.4)
+    s8 ~ Uniform(0.5, 1.0)
+    h ~ Uniform(0.5, 0.9)
+    cosmology = LimberJack.Cosmology(Ωm, 0.05, h, 0.96, s8,
+                                     tk_mode="EisHu",
+                                     Pk_mode="Halofit")
+    tg = NumberCountsTracer(cosmology, zs, nz, 2.)
+    ts = WeakLensingTracer(cosmology, zs, nz)
+    predictions = [angularCℓ(cosmology, tg, ts, ℓ) for ℓ in ell]
+    data ~ MvNormal(predictions, 0.01.*data)
+end;
+
+iterations = 10
+step_size = 0.05
+samples_per_step = 5
+# Start sampling.
+
+folname = string("Parallelization_test_", "stpsz_", step_size, "_smpls_", samples_per_step)
+if isdir(folname)
+    println("Folder already exists")
+    if isfile("chain.jls")
+        println("Restarting from past chain")
+        past_chain = read("chain.jls", Chains)
+        new_chain = sample(model(data), HMC(step_size, samples_per_step),
+                           MCMCDistributed(), iterations, cores, progress=true;
+                           save_state=true, resume_from=past_chain)
+    end
+else
+    mkdir(folname)
+    println("Created new folder")
+    new_chain = sample(model(data), HMC(step_size, samples_per_step),
+                       MCMCDistributed(), iterations, cores, progress=true; save_state=true)
+end
+
+info = describe(new_chain)[1]
+fname_info = string("info.csv")
+CSV.write(joinpath(folname, fname_info), info)
+
+
+fname_jls = string("chain.jls")
+write(joinpath(folname, fname_jls), new_chain)
+    
+fname_csv = string("chain.csv")
+CSV.write(joinpath(folname, fname_csv), new_chain)
