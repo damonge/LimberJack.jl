96f128c2aa2ab046c930a8b508973b262e349431
jaimerzp <jaimerz011235813@gmail.com>
2022-05-09 12:00:47 +0200

    bugs

--- src/LimberJack.jl
+++ src/LimberJack.jl
@@ -6,7 +6,7 @@ export NumberCountsTracer, WeakLensingTracer, CMBLensingTracer, get_Fℓ
 export Cℓintegrand, angularCℓ, lin_Pk, nonlin_Pk
 export Theory, cls_meta
 
-using Interpolations, OrdinaryDiffEq, ForwardDiff, LinearAlgebra, QuadGK
+using Interpolations, OrdinaryDiffEq, ForwardDiff, LinearAlgebra, QuadGK, Trapz
 
 include("core.jl")
 include("tracers.jl")
