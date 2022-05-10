e3e6e22831a7f3590734958a2eb600cfd4ee5638
jaimerzp <jaimerz011235813@gmail.com>
2022-05-10 13:15:56 +0200

    new scripts

--- Notebooks/Test_distribution.jl
+++ Notebooks/Test_distribution.jl
@@ -12,35 +12,35 @@ data_vector = files["cls"]
 
 @model function model(data_vector; cov_tot=cov_tot)
     Ωm ~ Uniform(0.1, 0.6)
-    Ωb ~ Uniform(0.03, 0.07)
-    h ~ Uniform(0.6, 0.9)
+    Ωb = 0.05 #~ Uniform(0.03, 0.07)
+    h = 0.67 #~ Uniform(0.6, 0.9)
     s8 ~ Uniform(0.6, 1.0)
-    ns ~ Uniform(0.87, 1.07)
+    ns = 0.96 #~ Uniform(0.87, 1.07)
     
-    b0 ~ Uniform(0.8, 3.0)
-    b1 ~ Uniform(0.8, 3.0)
-    b2 ~ Uniform(0.8, 3.0)
-    b3 ~ Uniform(0.8, 3.0)
-    b4 ~ Uniform(0.8, 3.0)
+    b0 = 1.69 #1.41 #~ Uniform(1.0, 3.0)
+    b1 = 2.05 #1.62 #~ Uniform(1.0, 3.0)
+    b2 = 2.01 #1.60 #~ Uniform(1.0, 3.0)
+    b3 = 2.46 #1.92 #~ Uniform(1.0, 3.0)
+    b4 = 2.54 #2.00 #~ Uniform(1.0, 3.0)
     
-    dz_g0 ~ Normal(0.008, 0.007)
-    dz_g1 ~ Normal(-0.005, 0.007)
-    dz_g2 ~ Normal(0.006, 0.006)
-    dz_g3 ~ Normal(0.0, 0.010)
-    dz_g4 ~ Normal(0.0, 0.010)
+    dz_g0 = 0 #~ Normal(0.008, 0.007)
+    dz_g1 = 0 #~ Normal(-0.005, 0.007)
+    dz_g2 = 0 #~ Normal(0.006, 0.006)
+    dz_g3 = 0 #~ Normal(0.0, 0.010)
+    dz_g4 = 0 #~ Normal(0.0, 0.010)
     
-    dz_k0 ~ Normal(-0.001, 0.016)
-    dz_k1 ~ Normal(-0.019, 0.013)
-    dz_k2 ~ Normal(-0.009, 0.011)
-    dz_k3 ~ Normal(-0.018, 0.022)
+    dz_k0 = 0 #~ Normal(-0.001, 0.016)
+    dz_k1 = 0 #~ Normal(-0.019, 0.013)
+    dz_k2 = 0 #~ Normal(-0.009, 0.011)
+    dz_k3 = 0 #~ Normal(-0.018, 0.022)
     
-    m0 ~ Normal(0.0, 0.035)
-    m1 ~ Normal(0.0, 0.035)
-    m2 ~ Normal(0.0, 0.035)
-    m3 ~ Normal(0.0, 0.035)
+    m0 = 0 #~ Normal(0.012, 0.023) #Normal(0.0, 0.035)
+    m1 = 0 #~ Normal(0.012, 0.023) #Normal(0.0, 0.035)
+    m2 = 0 #~ Normal(0.012, 0.023) #Normal(0.0, 0.035)
+    m3 = 0 #~ Normal(0.012, 0.023) #Normal(0.0, 0.035)
     
-    A_IA ~ Uniform(-5, 5) 
-    alpha_IA ~ Uniform(-5, 5)
+    A_IA = 0 #~ Uniform(-5, 5) 
+    alpha_IA = 0 #~ Uniform(-5, 5)
 
     nuisances = Dict("b0" => b0,
                      "b1" => b1,
@@ -71,31 +71,21 @@ data_vector = files["cls"]
     data_vector ~ MvNormal(theory, cov_tot)
 end;
 
-iterations = 5000
+iterations = 1000
 TAP = 0.60
-adaptation = 1000
+adaptation = 500
+nchains = 4
 
 # Start sampling.
 folpath = "../chains"
-folname = string("DES_full_NUTS_", "TAP", TAP)
+folname = string("test_distribution_", "TAP", TAP)
 folname = joinpath(folpath, folname)
-if isdir(folname)
-    println("Folder already exists")
-    if isfile(joinpath(folname, "chain.jls"))
-        println("Restarting from past chain")
-        past_chain = read(joinpath(folname, "chain.jls"), Chains)
-        new_chain = sample(model(data_vector), NUTS(adaptation, TAP), iterations,
-                           progress=true; save_state=true, resume_from=past_chain)
-    else
-        new_chain = sample(model(data_vector), NUTS(adaptation, TAP),
-                           iterations, progress=true; save_state=true)
-    end
-else
-    mkdir(folname)
-    println("Created new folder")
-    new_chain = sample(model(data_vector), NUTS(adaptation, TAP),
-                       iterations, progress=true; save_state=true)
-end
+mkdir(folname)
+println("Created new folder")
+
+new_chains = mapreduce(c ->sample(model(data_vector), NUTS(adaptation, TAP),
+                   iterations, progress=true; save_state=true), chainscat, 1:nchains)
+
 
 summary = describe(new_chain)[1]
 fname_summary = string("summary", now(), ".csv")
