73db06a4a91126675effbd995c9518ccfa815f69
jaimerzp <jaimerz011235813@gmail.com>
2022-05-11 12:33:09 +0200

    bug

--- src/spectra.jl
+++ src/spectra.jl
@@ -21,12 +21,14 @@ function angularCℓs(cosmo::Cosmology, t1::Tracer, t2::Tracer, ℓs::Vector{Flo
     logks = LinRange(log(10^-4),log(10^2), res)
     dlogk = logks[2]-logks[1]
     Cℓs = Vector{Float64}(undef, length(ℓs))
-    for ℓ in ℓs
+    for i in 1:length(ℓs)
+        ℓ = ℓs[i]
         integrand = [Cℓintegrand(cosmo, t1, t2, logk, ℓ)/(ℓ+0.5) for logk in logks]
         Cℓ = sum(0.5 .* (integrand[1:res-1] .+ integrand[2:res]) .* dlogk)
         fℓ1 = get_Fℓ(t1, ℓ)
         fℓ2 = get_Fℓ(t2, ℓ)
-        Cℓ *= fℓ1 * fℓ2
+        Cℓ = Cℓ * fℓ1 * fℓ2
+        Cℓs[i] = Cℓ
     end
     return Cℓs
 end
