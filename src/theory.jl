f3078dc74e0940e28d336fe7684529eb8c544ea1
jaimerzp <jaimerz011235813@gmail.com>
2022-05-12 10:55:51 +0200

    bug

--- src/theory.jl
+++ src/theory.jl
@@ -75,7 +75,7 @@ function Theory(cosmology, Nuisances, cls_meta, files)
         ls = files[string("ls_", pair[1], pair[2], pair[3], pair[4])]
         tracer1 = tracers[ids[1]]
         tracer2 = tracers[ids[2]]
-        Cl = [angularCℓs(cosmology, tracer1, tracer2, l) for l in ls]
+        Cl = [angularCℓ(cosmology, tracer1, tracer2, l) for l in ls]
         push!(Cls, Cl)
     end
     return Cls
