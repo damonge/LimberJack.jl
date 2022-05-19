dc9d5b0617644ef2b0705c618255a4a0809d6c3e
jaimerzp <jaimerz011235813@gmail.com>
2022-05-19 17:46:09 +0100

    new scripts

--- Notebooks/DES_cosmo_bias_dz_IA.jl
+++ Notebooks/DES_cosmo_bias_dz_IA.jl
@@ -1,49 +1,38 @@
-using Distributed
 using Turing
+using LimberJack
+using CSV
+using NPZ
+using FITSIO
+using Dates
 
-@everywhere using Turing
-@everywhere using LimberJack
-@everywhere using CSV
-@everywhere using NPZ
-@everywhere using FITSIO
-@everywhere using Dates
+println(Threads.nthreads())
 
-@everywhere println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
+files = npzread("../data/DESY1_cls/Cls_meta.npz")
+Cls_meta = cls_meta(files)
+cov_tot = files["cov"]
+data_vector = files["cls"]
 
-@everywhere files = npzread("../data/DESY1_cls/Cls_meta.npz")
-@everywhere Cls_meta = cls_meta(files)
-@everywhere cov_tot = files["cov"]
-@everywhere data_vector = files["cls"]
-
-@everywhere @model function model(data_vector; cov_tot=cov_tot)
+@model function model(data_vector; cov_tot=cov_tot)
     Ωm ~ Uniform(0.1, 0.6)
     Ωb ~ Uniform(0.03, 0.07)
     h ~ Uniform(0.6, 0.9)
     s8 ~ Uniform(0.6, 1.0)
     ns ~ Uniform(0.87, 1.07)
     
-    b0 ~ Uniform(0.8, 3.0)
-    b1 ~ Uniform(0.8, 3.0)
-    b2 ~ Uniform(0.8, 3.0)
-    b3 ~ Uniform(0.8, 3.0)
-    b4 ~ Uniform(0.8, 3.0)
-    
     dz_g0 ~ Normal(0.008, 0.007)
     dz_g1 ~ Normal(-0.005, 0.007)
     dz_g2 ~ Normal(0.006, 0.006)
     dz_g3 ~ Normal(0.0, 0.010)
     dz_g4 ~ Normal(0.0, 0.010)
-    
     dz_k0 ~ Normal(-0.001, 0.016)
     dz_k1 ~ Normal(-0.019, 0.013)
     dz_k2 ~ Normal(-0.009, 0.011)
     dz_k3 ~ Normal(-0.018, 0.022)
-    
-    m0 ~ Normal(0.0, 0.035)
-    m1 ~ Normal(0.0, 0.035)
-    m2 ~ Normal(0.0, 0.035)
-    m3 ~ Normal(0.0, 0.035)
-    
+    b0 ~ Uniform(0.8, 3.0)
+    b1 ~ Uniform(0.8, 3.0)
+    b2 ~ Uniform(0.8, 3.0)
+    b3 ~ Uniform(0.8, 3.0)
+    b4 ~ Uniform(0.8, 3.0)
     A_IA ~ Uniform(-5, 5) 
     alpha_IA ~ Uniform(-5, 5)
 
@@ -61,10 +50,6 @@ using Turing
                      "dz_k1" => dz_k1,
                      "dz_k2" => dz_k2,
                      "dz_k3" => dz_k3,
-                     "m0" => m0,
-                     "m1" => m1,
-                     "m2" => m2,
-                     "m3" => m3,
                      "A_IA" => A_IA,
                      "alpha_IA" => alpha_IA)
     
@@ -72,18 +57,17 @@ using Turing
                                      tk_mode="EisHu",
                                      Pk_mode="Halofit")
     
-    theory = Theory(cosmology, nuisances, Cls_meta, files)
+    theory = vcat(Theory(cosmology, nuisances, Cls_meta, files)...)
     data_vector ~ MvNormal(theory, cov_tot)
 end;
 
-iterations = 2
-TAP = 0.1
-adaptation = 2
-nchains = nprocs()
+iterations = 5000
+TAP = 0.60
+adaptation = 1000
 
 # Start sampling.
 folpath = "../chains"
-folname = string("DES_full_distributed_", "TAP", TAP)
+folname = string("DES_cosmo_bias_IA_", "TAP", TAP)
 folname = joinpath(folpath, folname)
 
 if isdir(folname)
@@ -94,14 +78,11 @@ end
 mkdir(folname)
 println("Created new folder")
 
-new_chain = sample(model(data_vector), NUTS(adaptation, TAP), MCMCDistributed(),
-                   iterations, nchains, progress=true; save_state=true)
+new_chain = sample(model(data_vector), NUTS(adaptation, TAP),
+                   iterations, progress=true; save_state=true)
 
-#= 
-new_chain = sample(model(data_vector), NUTS(adaptation, TAP), MCMCThreads(),
-                   iterations, nchains, progress=true; save_state=true,
-                   resume_from=past_chain)
-=#
+#new_chain = sample(model(data_vector), NUTS(adaptation, TAP), iterations,
+#                   progress=true; save_state=true, resume_from=past_chain)
 
 summary = describe(new_chain)[1]
 fname_summary = string("summary", now(), ".csv")
@@ -109,6 +90,6 @@ CSV.write(joinpath(folname, fname_summary), summary)
 
 fname_jls = string("chain.jls")
 write(joinpath(folname, fname_jls), new_chain)
-
+    
 fname_csv = string("chain_", now(), ".csv")
 CSV.write(joinpath(folname, fname_csv), new_chain)
