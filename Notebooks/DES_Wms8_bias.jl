9eb7523172ca00efe5d0e026fc6b55919c477204
jaimerzp <jaimerz011235813@gmail.com>
2022-06-14 11:14:26 +0100

    first commit

---
+++ Notebooks/DES_Wms8_bias.jl
@@ -0,0 +1,76 @@
+using Turing
+using LimberJack
+using CSV
+using NPZ
+using FITSIO
+using Dates
+
+println(Threads.nthreads())
+
+files = npzread("../data/DESY1_cls/Cls_meta.npz")
+Cls_meta = cls_meta(files)
+cov_tot = files["cov"]
+data_vector = files["cls"]
+
+@model function model(data_vector; cov_tot=cov_tot)
+    Ωm ~ Uniform(0.1, 0.6)
+    Ωb = 0.05 
+    h = 0.67 
+    s8 ~ Uniform(0.6, 1.0)
+    ns = 0.96 
+    
+    b0 ~ Uniform(0.8, 3.0)
+    b1 ~ Uniform(0.8, 3.0)
+    b2 ~ Uniform(0.8, 3.0)
+    b3 ~ Uniform(0.8, 3.0)
+    b4 ~ Uniform(0.8, 3.0)
+    
+    nuisances = Dict("b0" => b0,
+                     "b1" => b1,
+                     "b2" => b2,
+                     "b3" => b3,
+                     "b4" => b4)
+    
+    cosmology = LimberJack.Cosmology(Ωm, Ωb, h, ns, s8,
+                                     tk_mode="EisHu",
+                                     Pk_mode="Halofit")
+    
+    theory = vcat(Theory(cosmology, nuisances, Cls_meta, files)...)
+    data_vector ~ MvNormal(theory, cov_tot)
+end;
+
+iterations = 2000
+TAP = 0.60
+adaptation = 500
+
+# Start sampling.
+folpath = "../chains"
+folname = string("DES_Wms8_bias_", "TAP", TAP)
+folname = joinpath(folpath, folname)
+if isdir(folname)
+    println("Folder already exists")
+    if isfile(joinpath(folname, "chain.jls"))
+        println("Restarting from past chain")
+        past_chain = read(joinpath(folname, "chain.jls"), Chains)
+        new_chain = sample(model(data_vector), NUTS(adaptation, TAP), iterations,
+                           progress=true; save_state=true, resume_from=past_chain)
+    else
+        new_chain = sample(model(data_vector), NUTS(adaptation, TAP),
+                           iterations, progress=true; save_state=true)
+    end
+else
+    mkdir(folname)
+    println("Created new folder")
+    new_chain = sample(model(data_vector), NUTS(adaptation, TAP),
+                       iterations, progress=true; save_state=true)
+end
+
+summary = describe(new_chain)[1]
+fname_summary = string("summary", now(), ".csv")
+CSV.write(joinpath(folname, fname_summary), summary)
+
+fname_jls = string("chain.jls")
+write(joinpath(folname, fname_jls), new_chain)
+    
+fname_csv = string("chain_", now(), ".csv")
+CSV.write(joinpath(folname, fname_csv), new_chain)
