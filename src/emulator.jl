bbeb0cd53eea3ffe12b936566f8221d668250efc
jaimerzp <jaimerz011235813@gmail.com>
2022-07-05 18:28:56 +0100

    working

--- src/emulator.jl
+++ src/emulator.jl
@@ -1,16 +1,51 @@
-function interpolate(x_old, y_old)
-    """Function to interpolate the power spectrum along the redshift axis
+function x_transformation(point, x_arr)
+    """Pre-whiten the input parameters.
+
     Args:
-        inputs (list): x values, y values and new values of x
+        point (torch.tensor): the input parameters.
+
     Returns:
-        np.ndarray: an array of the interpolated power spectra
+        torch.tensor: the pre-whitened parameters.
     """
 
-    itp = LinearInterpolation(log.(x_old), log.(y_old))
+    N, M = size(x_arr)
+    cov_train = cov(x_arr)
+    chol_train = cholesky(cov_train).U'
+    mean_train = mean(x_arr, dims=1)
+    # calculate the transformed training points
+    transformed = inv(chol_train) * (point .- mean_train)'
+
+    return transformed
+end
+
+function y_transformation(point, y_arr)
+    """Transform the outputs.
+
+    Args:
+        yvalues (np.ndarray): the values to be transformed
+
+    Returns:
+        np.ndarray: the transformed outputs
+    """
+    ylog = log.(y_arr)
+    ymean = mean(ylog, dims=1)
+    ystd = std(ylog, dims=1)
+    return ((point .- ymean) ./ ystd)'
+end
+
+function inv_y_transformation(point, ylog)
+    """Transform the outputs.
+
+    Args:
+        yvalues (np.ndarray): the values to be transformed
+
+    Returns:
+        np.ndarray: the transformed outputs
+    """
     
-    return (y_new) -> let
-        return itp(log.(y_new))
-    end
+    ymean = mean(ylog, dims=1)
+    ystd = std(ylog, dims=1)
+    return @.(exp(ylog * ystd + ymean))
 end
 
 function get_kernel(arr1, arr2, hyper)
@@ -24,93 +59,31 @@ function get_kernel(arr1, arr2, hyper)
         np.ndarray: a tensor of size [N x M] containing the kernel matrix.
     """
     
-    arr1_w = @.(arr1/exp(hyper[2:]))
-    arr2_w = @.(arr2/exp(hyper[2:]))
+    arr1_w = @.(arr1/exp(hyper[2:6]))
+    arr2_w = @.(arr2/exp(hyper[2:6]))
     
     # compute the pairwise distance
-    term1 = @.(sum(exp(arr1_w^2), 1))
-    term2 = 2 * (arr1 * arr2)
-    term3 = @.(sum(exp(arr2^2), 1))
-    dist = term1 - term2 + term3'
-    
+    term1 = sum(arr1_w.^2, dims=1)
+    term2 = 2 * arr1_w' * arr2_w
+    term3 = sum(arr2_w.^2, dims=1)'
+    dist = term1 - term2' .+ term3
+
     # compute the kernel
-    kernel = np.exp(hyper[1]) * np.exp(-0.5 * dist)
+    kernel = @.(exp(hyper[1]) * exp(-0.5 * dist))
 
     return kernel
 end
 
-
-function emulated_Pk(testpoint)
-    """Compute the mean prediction of the GP.
-
-    Args:
-        testpoint (np.ndarray): the test points.
-
-    Returns:
-        np.ndarray: the mean prediction.
-    """
-
-    testpoint = x_transformation(testpoint)
-    preds = []
-
-    for i in 1:ngps
-        kernel = get_kernel(xtrain, testpoint, hyper)
-        mean = kernel .* alphas[i]
-        push!(preds, mean)
-
-    preds = np.array(preds) 
-    preds = inv_y_transformation(preds)
-
-    return preds
-end
-
-function x_transformation(x_arr)
-    """Pre-whiten the input parameters.
-
-    Args:
-        point (torch.tensor): the input parameters.
-
-    Returns:
-        torch.tensor: the pre-whitened parameters.
-    """
-
-    N, M = size(x_arr)
-    cov_train = cov(x_arr)
-    chol_train = Cholesky(cov_train)
-    mean_train = mean(x_arr, 1)
-
-    # calculate the transformed training points
-    transformed = inv(self.chol_train) .* (point .- mean_train)
-
-    return transformed
-end
-
-function y_transformation(y_arr)
-    """Transform the outputs.
-
-    Args:
-        yvalues (np.ndarray): the values to be transformed
-
-    Returns:
-        np.ndarray: the transformed outputs
-    """
-    ylog = log.(y_arr)
-    ymean = mean(ylog, axis=0)
-    ystd = std(ylog, axis=0)
-    return (ylog - ymean) / ystd
-end
-
-function inv_y_transformation(ylog)
-    """Transform the outputs.
-
-    Args:
-        yvalues (np.ndarray): the values to be transformed
-
-    Returns:
-        np.ndarray: the transformed outputs
-    """
+function get_Pk(params_input, x_input, y_input, alphas)
+    params = x_transformation(params_input)
+    x_train = x_transformation(x_input)
+    y_train = x_transformation(y_input)
     
-    ymean = mean(ylog, axis=0)
-    ystd = std(ylog, axis=0)
-    return @.(exp(ylog * ystd + ymean))
+    preds = []
+    for i in 1:40
+        kernel = get_kernel(xtrain, param, hyper[i, :])
+        mean = dot(vec(kernel), vec(alphas[i,:]))
+        push!(preds, mean)
+    end
+    return vec(inv_y_transformation(preds, y_train))
 end
