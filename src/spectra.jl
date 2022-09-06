cd71d90b7e8295d30723337fa73563d9cf5a8920
jaimerzp <jaimerz011235813@gmail.com>
2022-09-06 11:27:19 +0200

    spectra docs

--- src/spectra.jl
+++ src/spectra.jl
@@ -1,3 +1,20 @@
+"""
+    Cℓintegrand(cosmo::Cosmology, t1::Tracer, t2::Tracer, logk, ℓ)
+
+Returns the integrand of the angular power spectrum. 
+
+Arguments:
+
+- `cosmo::Cosmology` : cosmology structure.
+- `t1::Tracer` : tracer structure.
+- `t2::Tracer` : tracer structure.
+- `logk::Vector{Float}` : log scale array.
+- `ℓ::Float` : multipole.
+
+Returns:
+- `integrand::Vector{Real}` : integrand of the angular power spectrum.
+
+"""
 function Cℓintegrand(cosmo::Cosmology,
                      t1::Tracer,
                      t2::Tracer,
@@ -24,6 +41,22 @@ function Cℓintegrand(cosmo::Cosmology,
     return k*w1*w2*pk
 end
 
+"""
+    angularCℓs(cosmo::Cosmology, t1::Tracer, t2::Tracer, ℓs)
+
+Returns the angular power spectrum. 
+
+Arguments:
+
+- `cosmo::Cosmology` : cosmology structure.
+- `t1::Tracer` : tracer structure.
+- `t2::Tracer` : tracer structure.
+- `ℓs::Vector{Float}` : multipole array.
+
+Returns:
+- `Cℓs::Vector{Real}` : angular power spectrum.
+
+"""
 function angularCℓs(cosmo::Cosmology, t1::Tracer, t2::Tracer, ℓs)
     # OPT: we are not optimizing the limits of integration
     cosmo_type = cosmo.settings.cosmo_type
