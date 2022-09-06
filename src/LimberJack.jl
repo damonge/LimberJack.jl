b74f6a3b2122fffe09e8412b3321310f340e1395
jaimerzp <jaimerz011235813@gmail.com>
2022-09-06 17:00:04 +0200

    reordering stuff in LimberJack

--- src/LimberJack.jl
+++ src/LimberJack.jl
@@ -1,21 +1,22 @@
 module LimberJack
 
-export Settings, CosmoPar, Cosmology
-export Ez, Hmpc, comoving_radial_distance, growth_factor
+export Settings, CosmoPar, Cosmology, Ez, Hmpc, comoving_radial_distance, growth_factor
+export get_emulated_log_pk0,
+export Emulator, get_emulated_log_pk0
 export NumberCountsTracer, WeakLensingTracer, CMBLensingTracer, get_Fℓ
 export angularCℓs, angularCℓ, lin_Pk, nonlin_Pk
 export Theory, get_nzs
-export get_emulated_log_pk0, Emulator
 
 using Interpolations, OrdinaryDiffEq, ForwardDiff 
 using LinearAlgebra, Statistics, Trapz, QuadGK, NPZ
 
 include("core.jl")
+include("emulator.jl")
+include("halofit.jl")
 include("tracers.jl")
 include("spectra.jl")
-include("halofit.jl")
 include("theory.jl")
-include("emulator.jl")
+
 
 
 end
