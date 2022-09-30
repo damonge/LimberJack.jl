15246f77abb5a1326678f7e9cd0b2213861138ff
jaimerzp <jaimerz011235813@gmail.com>
2022-09-30 15:09:28 +0100

    less res

--- test/runtests.jl
+++ test/runtests.jl
@@ -60,10 +60,10 @@ np = pyimport("numpy")
                                                   Omega_g=0, Omega_k=0)
         cosmo_emul = Cosmology(0.30, 0.045, 0.67, 0.96, 0.81,
                                nk=500, tk_mode="emulator")
-        cosmo_emul_bm = ccl.CosmologyCalculator(Omega_c=0.255, Omega_b=0.045, h=0.67, ns=0.96, sigma8=0.81,
-                                                Omega_g=0, Omega_k=0,
-                                                transfer_function="boltzmann_camb",
-                                                matter_power_spectrum="linear")
+        cosmo_emul_bm = ccl.Cosmology(Omega_c=0.255, Omega_b=0.045, h=0.67, n_s=0.96, sigma8=0.81,
+                                      Omega_g=0, Omega_k=0,
+                                      transfer_function="boltzmann_class",
+                                      matter_power_spectrum="linear")
         ks = [0.001, 0.01, 0.1, 1.0, 10.0]
         pk_BBKS = nonlin_Pk(cosmo_BBKS, ks, 0.0)
         pk_EisHu = nonlin_Pk(cosmo_EisHu, ks, 0.0)
