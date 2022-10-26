a07e8f1b0bae0b8307db3eead3e8d8c3bac11ad9
jaimerzp <jaimerz011235813@gmail.com>
2022-10-26 10:50:12 +0100

    new script

--- Notebooks/Nz_Omega.jl
+++ Notebooks/Nz_Omega.jl
@@ -12,7 +12,7 @@ using Distributed
 @everywhere println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
 
 @everywhere fol = "DESY1"
-@everywhere data_set = "wlwl_lite10"
+@everywhere data_set = "wlwl_lite20"
 @everywhere meta = np.load(string("../data/", fol, "/", data_set, "_meta.npz"))
 @everywhere files = npzread(string("../data/", fol, "/", data_set, "_files.npz"))
 
@@ -22,7 +22,7 @@ using Distributed
 @everywhere data_vector = pyconvert(Vector{Float64}, meta["cls"])
 @everywhere cov_tot = pyconvert(Matrix{Float64}, meta["cov"]);
 
-@everywhere nz_path = "../data/DESY1/lite10_nzs/"
+@everywhere nz_path = "../data/DESY1/lite20_nzs/"
 @everywhere zs_k0, nz_k0, cov_k0 = get_nzs(nz_path, "DESwl__0_e")
 @everywhere zs_k1, nz_k1, cov_k1 = get_nzs(nz_path, "DESwl__1_e")
 @everywhere zs_k2, nz_k2, cov_k2 = get_nzs(nz_path, "DESwl__2_e")
@@ -33,79 +33,32 @@ using Distributed
                                   pairs=pairs,
                                   idx=idx,
                                   cov_tot=cov_tot, 
-                                  files=files)
+                                  files=files) 
     Ωm ~ Uniform(0.2, 0.6)
     Ωb = 0.05 #~ Uniform(0.03, 0.07)
     h = 0.67 #~ Uniform(0.55, 0.91)
     ns = 0.96 #~ Uniform(0.87, 1.07)
     s8 = 0.81 #~ Uniform(0.6, 0.9)
     
+    cosmology = LimberJack.Cosmology(Ωm, Ωb, h, ns, s8,
+                                     tk_mode="EisHu",
+                                     Pk_mode="Halofit")
+    
     A_IA = 0.0 #~ Uniform(-5, 5) 
     alpha_IA = 0.0 #~ Uniform(-5, 5)
-    DESwl__0_e_nz_1 ~ TruncatedNormal(nz_k0[1], sqrt(cov_k0[1]), 0.0, 3.0)
-    DESwl__0_e_nz_2 ~ TruncatedNormal(nz_k0[2], sqrt(cov_k0[2]), 0.0, 3.0)
-    DESwl__0_e_nz_3 ~ TruncatedNormal(nz_k0[3], sqrt(cov_k0[3]), 0.0, 3.0)
-    DESwl__0_e_nz_4 ~ TruncatedNormal(nz_k0[4], sqrt(cov_k0[4]), 0.0, 3.0)
-    DESwl__0_e_nz_5 ~ TruncatedNormal(nz_k0[5], sqrt(cov_k0[5]), 0.0, 3.0)
-    DESwl__0_e_nz_6 ~ TruncatedNormal(nz_k0[6], sqrt(cov_k0[6]), 0.0, 3.0)
-    DESwl__0_e_nz_7 ~ TruncatedNormal(nz_k0[7], sqrt(cov_k0[7]), 0.0, 3.0)
-    DESwl__0_e_nz_8 ~ TruncatedNormal(nz_k0[8], sqrt(cov_k0[8]), 0.0, 3.0)
-    DESwl__0_e_nz_9 ~ TruncatedNormal(nz_k0[9], sqrt(cov_k0[9]), 0.0, 3.0)
-    DESwl__0_e_nz_10 ~ TruncatedNormal(nz_k0[10], sqrt(cov_k0[10]), 0.0, 3.0)
-    DESwl__0_e_nz = [DESwl__0_e_nz_1, DESwl__0_e_nz_2, DESwl__0_e_nz_3,
-                     DESwl__0_e_nz_4, DESwl__0_e_nz_5, DESwl__0_e_nz_6,
-                     DESwl__0_e_nz_7, DESwl__0_e_nz_8, DESwl__0_e_nz_9,
-                     DESwl__0_e_nz_10]
     
-    DESwl__1_e_nz_1 ~ TruncatedNormal(nz_k1[1], sqrt(cov_k1[1]), 0.0, 3.0)
-    DESwl__1_e_nz_2 ~ TruncatedNormal(nz_k1[2], sqrt(cov_k1[2]), 0.0, 3.0)
-    DESwl__1_e_nz_3 ~ TruncatedNormal(nz_k1[3], sqrt(cov_k1[3]), 0.0, 3.0)
-    DESwl__1_e_nz_4 ~ TruncatedNormal(nz_k1[4], sqrt(cov_k1[4]), 0.0, 3.0)
-    DESwl__1_e_nz_5 ~ TruncatedNormal(nz_k1[5], sqrt(cov_k1[5]), 0.0, 3.0)
-    DESwl__1_e_nz_6 ~ TruncatedNormal(nz_k1[6], sqrt(cov_k1[6]), 0.0, 3.0)
-    DESwl__1_e_nz_7 ~ TruncatedNormal(nz_k1[7], sqrt(cov_k1[7]), 0.0, 3.0)
-    DESwl__1_e_nz_8 ~ TruncatedNormal(nz_k1[8], sqrt(cov_k1[8]), 0.0, 3.0)
-    DESwl__1_e_nz_9 ~ TruncatedNormal(nz_k1[9], sqrt(cov_k1[9]), 0.0, 3.0)
-    DESwl__1_e_nz_10 ~ TruncatedNormal(nz_k0[10], sqrt(cov_k1[10]), 0.0, 3.0)
-    DESwl__1_e_nz = [DESwl__1_e_nz_1, DESwl__1_e_nz_2, DESwl__1_e_nz_3,
-                     DESwl__1_e_nz_4, DESwl__1_e_nz_5, DESwl__1_e_nz_6,
-                     DESwl__1_e_nz_7, DESwl__1_e_nz_8, DESwl__1_e_nz_9,
-                     DESwl__1_e_nz_10]
-    
-    DESwl__2_e_nz_1 ~ TruncatedNormal(nz_k2[1], sqrt(cov_k2[1]), 0.0, 3.0)
-    DESwl__2_e_nz_2 ~ TruncatedNormal(nz_k2[2], sqrt(cov_k2[2]), 0.0, 3.0)
-    DESwl__2_e_nz_3 ~ TruncatedNormal(nz_k2[3], sqrt(cov_k2[3]), 0.0, 3.0)
-    DESwl__2_e_nz_4 ~ TruncatedNormal(nz_k2[4], sqrt(cov_k2[4]), 0.0, 3.0)
-    DESwl__2_e_nz_5 ~ TruncatedNormal(nz_k2[5], sqrt(cov_k2[5]), 0.0, 3.0)
-    DESwl__2_e_nz_6 ~ TruncatedNormal(nz_k2[6], sqrt(cov_k2[6]), 0.0, 3.0)
-    DESwl__2_e_nz_7 ~ TruncatedNormal(nz_k2[7], sqrt(cov_k2[7]), 0.0, 3.0)
-    DESwl__2_e_nz_8 ~ TruncatedNormal(nz_k2[8], sqrt(cov_k2[8]), 0.0, 3.0)
-    DESwl__2_e_nz_9 ~ TruncatedNormal(nz_k2[9], sqrt(cov_k2[9]), 0.0, 3.0)
-    DESwl__2_e_nz_10 ~ TruncatedNormal(nz_k2[10], sqrt(cov_k2[10]), 0.0, 3.0)
-    DESwl__2_e_nz = [DESwl__2_e_nz_1, DESwl__2_e_nz_2, DESwl__2_e_nz_3,
-                     DESwl__2_e_nz_4, DESwl__2_e_nz_5, DESwl__2_e_nz_6,
-                     DESwl__2_e_nz_7, DESwl__2_e_nz_8, DESwl__2_e_nz_9,
-                     DESwl__2_e_nz_10]
-    
-    DESwl__3_e_nz_1 ~ TruncatedNormal(nz_k3[1], sqrt(cov_k3[1]), 0.0, 3.0)
-    DESwl__3_e_nz_2 ~ TruncatedNormal(nz_k3[2], sqrt(cov_k3[2]), 0.0, 3.0)
-    DESwl__3_e_nz_3 ~ TruncatedNormal(nz_k3[3], sqrt(cov_k3[3]), 0.0, 3.0)
-    DESwl__3_e_nz_4 ~ TruncatedNormal(nz_k3[4], sqrt(cov_k3[4]), 0.0, 3.0)
-    DESwl__3_e_nz_5 ~ TruncatedNormal(nz_k3[5], sqrt(cov_k3[5]), 0.0, 3.0)
-    DESwl__3_e_nz_6 ~ TruncatedNormal(nz_k3[6], sqrt(cov_k3[6]), 0.0, 3.0)
-    DESwl__3_e_nz_7 ~ TruncatedNormal(nz_k3[7], sqrt(cov_k3[7]), 0.0, 3.0)
-    DESwl__3_e_nz_8 ~ TruncatedNormal(nz_k3[8], sqrt(cov_k3[8]), 0.0, 3.0)
-    DESwl__3_e_nz_9 ~ TruncatedNormal(nz_k3[9], sqrt(cov_k3[9]), 0.0, 3.0)
-    DESwl__3_e_nz_10 ~ TruncatedNormal(nz_k3[10], sqrt(cov_k3[10]), 0.0, 3.0)
-    DESwl__3_e_nz = [DESwl__3_e_nz_1, DESwl__3_e_nz_2, DESwl__3_e_nz_3,
-                     DESwl__3_e_nz_4, DESwl__3_e_nz_5, DESwl__3_e_nz_6,
-                     DESwl__3_e_nz_7, DESwl__3_e_nz_8, DESwl__3_e_nz_9,
-                     DESwl__3_e_nz_10]
+    n = length(nz_k0)
+    DESwl__0_e_nz = zeros(cosmology.settings.cosmo_type, n)
+    DESwl__1_e_nz = zeros(cosmology.settings.cosmo_type, n)
+    DESwl__2_e_nz = zeros(cosmology.settings.cosmo_type, n)
+    DESwl__3_e_nz = zeros(cosmology.settings.cosmo_type, n)
+    for i in 1:n
+        DESwl__0_e_nz[i] ~ TruncatedNormal(nz_k0[i], sqrt(cov_k0[i]), 0.0, 3.0) 
+        DESwl__1_e_nz[i] ~ TruncatedNormal(nz_k1[i], sqrt(cov_k1[i]), 0.0, 3.0) 
+        DESwl__2_e_nz[i] ~ TruncatedNormal(nz_k2[i], sqrt(cov_k2[i]), 0.0, 3.0) 
+        DESwl__3_e_nz[i] ~ TruncatedNormal(nz_k3[i], sqrt(cov_k3[i]), 0.0, 3.0) 
+    end
 
-    #DESwl__0_e_nz ~ MvNormal(nz_k0, cov_k0)
-    #DESwl__1_e_nz ~ MvNormal(nz_k1, cov_k1)
-    #DESwl__2_e_nz ~ MvNormal(nz_k2, cov_k2)
-    #DESwl__3_e_nz ~ MvNormal(nz_k3, cov_k3)
     DESwl__0_e_m = 0.012 #~ Normal(0.012, 0.023)
     DESwl__1_e_m = 0.012 #~ Normal(0.012, 0.023)
     DESwl__2_e_m = 0.012 #~ Normal(0.012, 0.023)
@@ -125,13 +78,8 @@ using Distributed
                      "DESwl__2_e_m" => DESwl__2_e_m,
                      "DESwl__3_e_m" => DESwl__3_e_m)
     
-    cosmology = LimberJack.Cosmology(Ωm, Ωb, h, ns, s8,
-                                     tk_mode="EisHu",
-                                     Pk_mode="Halofit")
-    
     theory = Theory(cosmology, tracers_names, pairs,
-                    idx, files;
-                    Nuisances=nuisances)
+                    idx, files; Nuisances=nuisances)
     data_vector ~ MvNormal(theory, cov_tot)
 end;
 
@@ -152,7 +100,7 @@ println("nchains ", nchains)
 
 # Start sampling.
 folpath = "../chains"
-folname = string("DES_wlwl_Nzs_Omega_", "TAP_", TAP)
+folname = string("DES_wlwl_Nzs0_Omega_", "TAP_", TAP)
 folname = joinpath(folpath, folname)
 
 if isdir(folname)
