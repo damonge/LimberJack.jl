ea1e12207dd9464eeb3cc289af16907fd09e16fd
Jaime Ruiz-Zapatero <Maquiner>
2022-11-07 09:24:11 +0000

    bugs

--- Notebooks/Nz_numerical.jl
+++ Notebooks/Nz_numerical.jl
@@ -53,10 +53,10 @@ using Distributed
     DESwl__2_e_nz = zeros(cosmology.settings.cosmo_type, n)
     DESwl__3_e_nz = zeros(cosmology.settings.cosmo_type, n)
     for i in 1:n
-        DESwl__0_e_nz[i] ~ TruncatedNormal(nz_k0[i], sqrt(diag(cov_k0[i])), 0.0, Inf) 
-        DESwl__1_e_nz[i] ~ TruncatedNormal(nz_k1[i], sqrt(diag(cov_k1[i])), 0.0, Inf) 
-        DESwl__2_e_nz[i] ~ TruncatedNormal(nz_k2[i], sqrt(diag(cov_k2[i])), 0.0, Inf) 
-        DESwl__3_e_nz[i] ~ TruncatedNormal(nz_k3[i], sqrt(diag(cov_k3[i])), 0.0, Inf) 
+        DESwl__0_e_nz[i] ~ TruncatedNormal(nz_k0[i], sqrt.(diag(cov_k0))[i], 0.0, 1) 
+        DESwl__1_e_nz[i] ~ TruncatedNormal(nz_k1[i], sqrt.(diag(cov_k1))[i], 0.0, 1) 
+        DESwl__2_e_nz[i] ~ TruncatedNormal(nz_k2[i], sqrt.(diag(cov_k2))[i], 0.0, 1) 
+        DESwl__3_e_nz[i] ~ TruncatedNormal(nz_k3[i], sqrt.(diag(cov_k3))[i], 0.0, 1) 
     end
 
     DESwl__0_e_m ~ Normal(0.012, 0.023)
