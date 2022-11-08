4cdc4dc23fa2836d5a263ad8b1e84700d7fb0281
Jaime Ruiz-Zapatero <Maquiner>
2022-11-08 15:56:33 +0000

    bug

--- Notebooks/DECALS_whitened_cosmo.jl
+++ Notebooks/DECALS_whitened_cosmo.jl
@@ -54,7 +54,7 @@ using Distributed
     
     theory = Theory(cosmology, tracers_names, pairs,
                     idx, files; Nuisances=nuisances)
-    data ~ MvNormal(theory /. errs, cov)
+    data ~ MvNormal(theory ./ errs, cov)
 end;
 
 cycles = 6
