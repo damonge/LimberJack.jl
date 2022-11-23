cb135107c761945eaaa180e8f0120b75f61d0fd0
jaimerzp <jaimerz011235813@gmail.com>
2022-11-23 15:44:53 +0000

    tutorial

--- data/make_data.py
+++ data/make_data.py
@@ -13,9 +13,13 @@ def get_type(name, mode="write"):
     else:
         return '0'
 
-s = sacc.Sacc().load_fits("LSST/cls_covG_lsst_clustering.fits")
-fname = "LSST/gcgc"
-with open(fname+".yml") as f:
+sacc_path = "FD/cls_FD_covG.fits"
+yaml_path = "DESY1/gcgc"
+nzs_path = None #"DESY1/binned_40_nzs/"
+fname = "DESY1/gcgc"
+
+s = sacc.Sacc().load_fits(sacc_path)
+with open(yaml_path+".yml") as f:
     config = yaml.safe_load(f)
 
 # Apply scale cuts
@@ -86,7 +90,7 @@ dict_save = {'tracers': tracers, 'pairs': pairs,
              'pairs_ids': pairs_ids, 'cls': cls, 'idx': idx,
              'cov': cov, 'inv_cov': inv_cov}
 
-np.savez(fname+"_Nzs_40_meta.npz", **dict_save)
+np.savez(fname+"_meta.npz", **dict_save)
 
 ###########
 
@@ -97,7 +101,6 @@ for pair, l in zip(pairs, ls):
     print(t1, t2, len(l))
     dict_save[f'ls_{t1}_{t2}'] = np.array(l)
 
-nzs_path = "DESY1/binned_40_nzs/"
 for name, tracer in s.tracers.items():
     name = name+'_'+get_type(name, mode="write")
     if name in tracers:
@@ -111,4 +114,4 @@ for name, tracer in s.tracers.items():
             dndz = nzs["dndz"]
             dict_save[f'nz_{name}'] = np.array([z, dndz])
 
-np.savez(fname+"_Nzs_40_files.npz", **dict_save)
+np.savez(fname+"_files.npz", **dict_save)
