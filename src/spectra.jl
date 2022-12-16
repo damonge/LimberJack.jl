a78cf12a2e7359bb3da487b155adce136ca4a368
jaimerz <jaimerz011235813@gmail.com>
2022-12-16 20:03:54 +0000

    changes

--- src/spectra.jl
+++ src/spectra.jl
@@ -1,18 +1,18 @@
 """
-    Cℓintegrand(cosmo::Cosmology, t1::AbstractInterpolation, t2::AbstractInterpolation, logk, ℓ)
+    Cℓintegrand(cosmo::Cosmology, t1::Tracer, t2::Tracer, logk, ℓ)
 Returns the integrand of the angular power spectrum. 
 Arguments:
 - `cosmo::Cosmology` : cosmology structure.
-- `t1::AbstractInterpolation` : tracer kernel.
-- `t2::AbstractInterpolation` : tracer kernel.
+- `t1::Tracer` : tracer structure.
+- `t2::Tracer` : tracer structure.
 - `logk::Vector{Float}` : log scale array.
 - `ℓ::Float` : multipole.
 Returns:
 - `integrand::Vector{Real}` : integrand of the angular power spectrum.
 """
 function Cℓintegrand(cosmo::Cosmology,
-                     t1::AbstractInterpolation,
-                     t2::AbstractInterpolation,
+                     t1::Tracer,
+                     t2::Tracer,
                      ℓ::Number)
 
     chis = zeros(cosmo.settings.cosmo_type, cosmo.settings.nk)
@@ -20,28 +20,23 @@ function Cℓintegrand(cosmo::Cosmology,
     chis .*= (chis .< cosmo.chi_max)
     z = cosmo.z_of_chi(chis)
 
-    w1 = t1(chis)
-    w2 = t2(chis) 
+    w1 = t1.wint(chis)
+    w2 = t2.wint(chis)
     pk = nonlin_Pk(cosmo, cosmo.ks, z)
-    
+
     return @. (cosmo.ks*w1*w2*pk)
 end
 
 """
     angularCℓs(cosmo::Cosmology, t1::Tracer, t2::Tracer, ℓs)
-
 Returns the angular power spectrum. 
-
 Arguments:
-
 - `cosmo::Cosmology` : cosmology structure.
 - `t1::Tracer` : tracer structure.
 - `t2::Tracer` : tracer structure.
 - `ℓs::Vector{Float}` : multipole array.
-
 Returns:
 - `Cℓs::Vector{Real}` : angular power spectrum.
-
 """
 function angularCℓs(cosmo::Cosmology, t1::Tracer, t2::Tracer, ℓs::Vector)
     # OPT: we are not optimizing the limits of integration
