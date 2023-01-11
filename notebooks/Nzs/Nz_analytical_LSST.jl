521171552ada5cec853fcbb61453782b93bebaec
jaimerz <jaimerz011235813@gmail.com>
2023-01-11 09:30:11 -0800

    Refactoring

--- notebooks/Nzs/Nz_analytical_LSST.jl
+++ notebooks/Nzs/Nz_analytical_LSST.jl
@@ -1,44 +1,43 @@
 using Distributed
 
-@everywhere using Turing
-@everywhere using LimberJack
-@everywhere using CSV
-@everywhere using NPZ
-@everywhere using FITSIO
-@everywhere using LinearAlgebra
-@everywhere using PythonCall
-@everywhere np = pyimport("numpy")
+@everywhere begin
+    using Turing
+    using LimberJack
+    using CSV
+    using NPZ
+    using YAML
+    using LinearAlgebra
+    using PythonCall
+    sacc = pyimport("sacc");
 
-@everywhere println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
+    println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
 
-@everywhere fol = "LSST"
-@everywhere data_set = "wlwl_Nzs_40"
-@everywhere meta = np.load(string("../../data/", fol, "/", data_set, "_meta.npz"))
-@everywhere files = npzread(string("../../data/", fol, "/", data_set, "_files.npz"))
+    sacc_path = "../../data/LSST/cls_covG_lsst.fits"
+    yaml_path = "../../data/DESY1/wlwl.yml"
+    sacc_file = sacc.Sacc().load_fits(sacc_path)
+    yaml_file = YAML.load_file(yaml_path)
+    nz_DESwl__0 = npzread(string(nz_path, "DESwl__0")
+    nz_DESwl__1 = npzread(string(nz_path, "DESwl__1")
+    nz_DESwl__2 = npzread(string(nz_path, "DESwl__2")
+    nz_DESwl__3 = npzread(string(nz_path, "DESwl__3")
+    meta, files = make_data(sacc_file, yaml_file;
+                            nz_DESwl__0=nz_DESwl__0,
+                            nz_DESwl__1=nz_DESwl__1,
+                            nz_DESwl__2=nz_DESwl__2,
+                            nz_DESwl__3=nz_DESwl__3)
 
-@everywhere names = pyconvert(Vector{String}, meta["names"])
-@everywhere types = pyconvert(Vector{String}, meta["types"])
-@everywhere pairs = pyconvert(Vector{Vector{String}}, meta["pairs"])
-@everywhere idx = pyconvert(Vector{Int}, meta["idx"])
-@everywhere data_vector = pyconvert(Vector{Float64}, meta["cls"])
-@everywhere cov_tot = npzread("../../data/DESY1/binned_40_nzs/wlwl_cov_marg_lsst.npz")["cov_marg"]
-@everywhere errs = sqrt.(diag(cov_tot))
-@everywhere fake_data = data_vector ./ errs
-@everywhere fake_cov = Hermitian(cov_tot ./ (errs * errs'));
+    data_vector = meta.data
+    cov_tot = npzread("../../data/DESY1/binned_40_nzs/wlwl_dz_cov_marg_lsst.npz")["cov_marg"]
 
-@everywhere nz_path = "../../data/DESY1/binned_40_nzs/"
-@everywhere zs_k0, nz_k0, cov_k0 = get_nzs(nz_path, "DESwl__0")
-@everywhere zs_k1, nz_k1, cov_k1 = get_nzs(nz_path, "DESwl__1")
-@everywhere zs_k2, nz_k2, cov_k2 = get_nzs(nz_path, "DESwl__2")
-@everywhere zs_k3, nz_k3, cov_k3 = get_nzs(nz_path, "DESwl__3")
+    errs = sqrt.(diag(cov_tot))
+    fake_data = data_vector ./ errs
+    fake_cov = Hermitian(cov_tot ./ (errs * errs'))
+end
 
 @everywhere @model function model(data;
-                                  names=names,
-                                  types=types,
-                                  pairs=pairs,
-                                  idx=idx,
-                                  cov=fake_cov, 
-                                  files=files) 
+                                  meta=meta,
+                                  files=files,
+                                  cov=fake_cov)
     Ωm ~ Uniform(0.2, 0.6)
     s8 ~ Uniform(0.6, 0.9)
     Ωb ~ Uniform(0.03, 0.07)
@@ -59,37 +58,30 @@ using Distributed
 
     nuisances = Dict("A_IA" => A_IA,
                      "alpha_IA" => alpha_IA,
-                     "DESwl__0_zs" => zs_k0,
-                     "DESwl__1_zs" => zs_k1,
-                     "DESwl__2_zs" => zs_k2,
-                     "DESwl__3_zs" => zs_k3,
-                     "DESwl__0_nz" => nz_k0,
-                     "DESwl__1_nz" => nz_k1,
-                     "DESwl__2_nz" => nz_k2,
-                     "DESwl__3_nz" => nz_k3,
                      "DESwl__0_e_m" => DESwl__0_e_m,
                      "DESwl__1_e_m" => DESwl__1_e_m,
                      "DESwl__2_e_m" => DESwl__2_e_m,
                      "DESwl__3_e_m" => DESwl__3_e_m)
     
-    theory = Theory(cosmology, names, types, pairs,
-                    idx, files; Nuisances=nuisances)
+    theory = Theory(cosmology, meta, files; Nuisances=nuisances)
     data ~ MvNormal(theory ./ errs, cov)
 end;
 
 cycles = 6
-steps = 50
 iterations = 250
-TAP = 0.60
-adaptation = 100
-init_ϵ = 0.05
 nchains = nprocs()
+
+adaptation = 250
+TAP = 0.65
+
+stats_model = model(fake_data)
+sampler = NUTS(adaptation, TAP)
+
 println("sampling settings: ")
 println("cycles ", cycles)
 println("iterations ", iterations)
 println("TAP ", TAP)
 println("adaptation ", adaptation)
-println("init_ϵ ", init_ϵ)
 println("nchains ", nchains)
 
 # Start sampling.
@@ -99,12 +91,13 @@ folname = joinpath(folpath, folname)
 
 if isdir(folname)
     fol_files = readdir(folname)
-    println("Found existing file")
+    println("Found existing file ", folname)
     if length(fol_files) != 0
         last_chain = last([file for file in fol_files if occursin("chain", file)])
         last_n = parse(Int, last_chain[7])
         println("Restarting chain")
     else
+        println("Starting new chain")
         last_n = 0
     end
 else
@@ -113,15 +106,15 @@ else
     last_n = 0
 end
 
-for i in (1+last_n):(last_n+cycles)
+for i in (1+last_n):(cycles+last_n)
     if i == 1
-        chain = sample(model(fake_data), NUTS(adaptation, TAP), MCMCDistributed(),
+        chain = sample(stats_model, sampler, MCMCDistributed(),
                        iterations, nchains, progress=true; save_state=true)
     else
         old_chain = read(joinpath(folname, string("chain_", i-1,".jls")), Chains)
-        chain = sample(model(fake_data), NUTS(adaptation, TAP), MCMCDistributed(),
+        chain = sample(stats_model, sampler, MCMCDistributed(),
                        iterations, nchains, progress=true; save_state=true, resume_from=old_chain)
-    end  
+    end
     write(joinpath(folname, string("chain_", i,".jls")), chain)
     CSV.write(joinpath(folname, string("chain_", i,".csv")), chain)
     CSV.write(joinpath(folname, string("summary_", i,".csv")), describe(chain)[1])
