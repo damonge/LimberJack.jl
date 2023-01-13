a6fdcb7a98e941a8f3fd4f8d32fc33738dcafb98
jaimerz <jaimerz011235813@gmail.com>
2023-01-13 08:49:20 -0800

    FD dense

--- notebooks/growth_with_rsds/FD.jl
+++ notebooks/growth_with_rsds/FD.jl
@@ -1,171 +1,174 @@
 using Distributed
 
-@everywhere using Turing
-@everywhere using LimberJack
-@everywhere using CSV
-@everywhere using NPZ
-@everywhere using FITSIO
-@everywhere using PythonCall
-@everywhere np = pyimport("numpy")
+@everywhere begin
+    using LinearAlgebra
+    using Turing
+    using AdvancedHMC
+    using LimberJack
+    using CSV
+    using YAML
+    using PythonCall
+    sacc = pyimport("sacc");
 
-@everywhere println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
+    println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
 
-@everywhere data_set = "FD"
-@everywhere meta = np.load(string("../data/", data_set, "/", data_set, "_meta.npz"))
-@everywhere files = npzread(string("../data/", data_set, "/", data_set, "_files.npz"))
+    sacc_path = "../../data/FD/cls_FD_covG.fits"
+    yaml_path = "../../data/FD/FD.yml"
+    sacc_file = sacc.Sacc().load_fits(sacc_path)
+    yaml_file = YAML.load_file(yaml_path)
+    meta, files = make_data(sacc_file, yaml_file)
 
-@everywhere tracers_names = pyconvert(Vector{String}, meta["tracers"])
-@everywhere pairs = pyconvert(Vector{Vector{String}}, meta["pairs"])
-@everywhere idx = pyconvert(Vector{Int}, meta["idx"])
-@everywhere data_vector = pyconvert(Vector{Float64}, meta["cls"])
-@everywhere cov_tot = pyconvert(Matrix{Float64}, meta["cov"]);
+    data_vector = meta.data
+    cov_tot = meta.cov
+    errs = sqrt.(diag(cov_tot))
+    fake_data = data_vector ./ errs
+    fake_cov = Hermitian(cov_tot ./ (errs * errs'));
+end
 
-
-@everywhere @model function model(data_vector;
-                                  tracers_names=tracers_names,
-                                  pairs=pairs,
-                                  idx=idx,
-                                  cov_tot=cov_tot, 
+@everywhere @model function model(data;
+                                  cov=fake_cov,
+                                  meta=meta,
                                   files=files)
-
     #KiDS priors
     Ωm ~ Uniform(0.2, 0.6)
     Ωb ~ Uniform(0.028, 0.065)
-    h ~ Uniform(0.64, 0.82)
-    s8 ~ Uniform(0.6, 0.9)
+    h ~ TruncatedNormal(72, 5, 0.64, 0.82)
+    s8 ~ Uniform(0.4, 1.2)
     ns ~ Uniform(0.84, 1.1)
     
-    DESgc__0_0_b ~ Uniform(0.8, 3.0)
-    DESgc__1_0_b ~ Uniform(0.8, 3.0)
-    DESgc__2_0_b ~ Uniform(0.8, 3.0)
-    DESgc__3_0_b ~ Uniform(0.8, 3.0)
-    DESgc__4_0_b ~ Uniform(0.8, 3.0)
-    DESgc__0_0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
-    DESgc__1_0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
-    DESgc__2_0_dz ~ TruncatedNormal(0.0, 0.006, -0.2, 0.2)
-    DESgc__3_0_dz ~ TruncatedNormal(0.0, 0.01, -0.2, 0.2)
-    DESgc__4_0_dz ~ TruncatedNormal(0.0, 0.01, -0.2, 0.2)
+    DESgc__0_b ~ Uniform(0.8, 3.0)
+    DESgc__1_b ~ Uniform(0.8, 3.0)
+    DESgc__2_b ~ Uniform(0.8, 3.0)
+    DESgc__3_b ~ Uniform(0.8, 3.0)
+    DESgc__4_b ~ Uniform(0.8, 3.0)
+    DESgc__0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
+    DESgc__1_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
+    DESgc__2_dz ~ TruncatedNormal(0.0, 0.006, -0.2, 0.2)
+    DESgc__3_dz ~ TruncatedNormal(0.0, 0.01, -0.2, 0.2)
+    DESgc__4_dz ~ TruncatedNormal(0.0, 0.01, -0.2, 0.2)
     
     A_IA ~ Uniform(-5, 5) 
     alpha_IA ~ Uniform(-5, 5)
 
-    DESwl__0_e_dz ~ TruncatedNormal(-0.001, 0.016, -0.2, 0.2)
-    DESwl__1_e_dz ~ TruncatedNormal(-0.019, 0.013, -0.2, 0.2)
-    DESwl__2_e_dz ~ TruncatedNormal(-0.009, 0.011, -0.2, 0.2)
-    DESwl__3_e_dz ~ TruncatedNormal(-0.018, 0.022, -0.2, 0.2)
-    DESwl__0_e_m ~ Normal(0.012, 0.023)
-    DESwl__1_e_m ~ Normal(0.012, 0.023)
-    DESwl__2_e_m ~ Normal(0.012, 0.023)
-    DESwl__3_e_m ~ Normal(0.012, 0.023)
+    DESwl__0_dz ~ TruncatedNormal(-0.001, 0.016, -0.2, 0.2)
+    DESwl__1_dz ~ TruncatedNormal(-0.019, 0.013, -0.2, 0.2)
+    DESwl__2_dz ~ TruncatedNormal(-0.009, 0.011, -0.2, 0.2)
+    DESwl__3_dz ~ TruncatedNormal(-0.018, 0.022, -0.2, 0.2)
+    DESwl__0_m ~ Normal(0.012, 0.023)
+    DESwl__1_m ~ Normal(0.012, 0.023)
+    DESwl__2_m ~ Normal(0.012, 0.023)
+    DESwl__3_m ~ Normal(0.012, 0.023)
 
-    eBOSS__0_0_b ~ Uniform(0.8, 5.0)
-    eBOSS__1_0_b ~ Uniform(0.8, 5.0)
+    eBOSS__0_b ~ Uniform(0.8, 5.0)
+    eBOSS__1_b ~ Uniform(0.8, 5.0)
     
-    DECALS__0_0_b ~ Uniform(0.8, 3.0)
-    DECALS__1_0_b ~ Uniform(0.8, 3.0)
-    DECALS__2_0_b ~ Uniform(0.8, 3.0)
-    DECALS__3_0_b ~ Uniform(0.8, 3.0)
-    DECALS__0_0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
-    DECALS__1_0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
-    DECALS__2_0_dz ~ TruncatedNormal(0.0, 0.006, -0.2, 0.2)
-    DECALS__3_0_dz ~ TruncatedNormal(0.0, 0.010, -0.2, 0.2)
+    DECALS__0_b ~ Uniform(0.8, 3.0)
+    DECALS__1_b ~ Uniform(0.8, 3.0)
+    DECALS__2_b ~ Uniform(0.8, 3.0)
+    DECALS__3_b ~ Uniform(0.8, 3.0)
+    DECALS__0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
+    DECALS__1_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
+    DECALS__2_dz ~ TruncatedNormal(0.0, 0.006, -0.2, 0.2)
+    DECALS__3_dz ~ TruncatedNormal(0.0, 0.010, -0.2, 0.2)
     
-    KiDS1000__0_e_dz ~ TruncatedNormal(0.0, 0.0106, -0.2, 0.2)
-    KiDS1000__1_e_dz ~ TruncatedNormal(0.0, 0.0113, -0.2, 0.2)
-    KiDS1000__2_e_dz ~ TruncatedNormal(0.0, 0.0118, -0.2, 0.2)
-    KiDS1000__3_e_dz ~ TruncatedNormal(0.0, 0.0087, -0.2, 0.2)
-    KiDS1000__4_e_dz ~ TruncatedNormal(0.0, 0.0097, -0.2, 0.2)
-    KiDS1000__0_e_m ~ Normal(0.0, 0.019)
-    KiDS1000__1_e_m ~ Normal(0.0, 0.020)
-    KiDS1000__2_e_m ~ Normal(0.0, 0.017)
-    KiDS1000__3_e_m ~ Normal(0.0, 0.012)
-    KiDS1000__4_e_m ~ Normal(0.0, 0.010)
+    KiDS1000__0_dz ~ TruncatedNormal(0.0, 0.0106, -0.2, 0.2)
+    KiDS1000__1_dz ~ TruncatedNormal(0.0, 0.0113, -0.2, 0.2)
+    KiDS1000__2_dz ~ TruncatedNormal(0.0, 0.0118, -0.2, 0.2)
+    KiDS1000__3_dz ~ TruncatedNormal(0.0, 0.0087, -0.2, 0.2)
+    KiDS1000__4_dz ~ TruncatedNormal(0.0, 0.0097, -0.2, 0.2)
+    KiDS1000__0_m ~ Normal(0.0, 0.019)
+    KiDS1000__1_m ~ Normal(0.0, 0.020)
+    KiDS1000__2_m ~ Normal(0.0, 0.017)
+    KiDS1000__3_m ~ Normal(0.0, 0.012)
+    KiDS1000__4_m ~ Normal(0.0, 0.010)
 
 
-    nuisances = Dict("DESgc__0_0_b" => DESgc__0_0_b,
-                     "DESgc__1_0_b" => DESgc__1_0_b,
-                     "DESgc__2_0_b" => DESgc__2_0_b,
-                     "DESgc__3_0_b" => DESgc__3_0_b,
-                     "DESgc__4_0_b" => DESgc__4_0_b,
-                     "DESgc__0_0_dz" => DESgc__0_0_dz,
-                     "DESgc__1_0_dz" => DESgc__1_0_dz,
-                     "DESgc__2_0_dz" => DESgc__2_0_dz,
-                     "DESgc__3_0_dz" => DESgc__3_0_dz,
-                     "DESgc__4_0_dz" => DESgc__4_0_dz,
+    nuisances = Dict("DESgc__0_b" => DESgc__0_b,
+                     "DESgc__1_b" => DESgc__1_b,
+                     "DESgc__2_b" => DESgc__2_b,
+                     "DESgc__3_b" => DESgc__3_b,
+                     "DESgc__4_b" => DESgc__4_b,
+                     "DESgc__0_dz" => DESgc__0_dz,
+                     "DESgc__1_dz" => DESgc__1_dz,
+                     "DESgc__2_dz" => DESgc__2_dz,
+                     "DESgc__3_dz" => DESgc__3_dz,
+                     "DESgc__4_dz" => DESgc__4_dz,
         
                      "A_IA" => A_IA,
                      "alpha_IA" => alpha_IA,
 
-                     "DESwl__0_e_dz" => DESwl__0_e_dz,
-                     "DESwl__1_e_dz" => DESwl__1_e_dz,
-                     "DESwl__2_e_dz" => DESwl__2_e_dz,
-                     "DESwl__3_e_dz" => DESwl__3_e_dz,
-                     "DESwl__0_e_m" => DESwl__0_e_m,
-                     "DESwl__1_e_m" => DESwl__1_e_m,
-                     "DESwl__2_e_m" => DESwl__2_e_m,
-                     "DESwl__3_e_m" => DESwl__3_e_m,
+                     "DESwl__0_dz" => DESwl__0_dz,
+                     "DESwl__1_dz" => DESwl__1_dz,
+                     "DESwl__2_dz" => DESwl__2_dz,
+                     "DESwl__3_dz" => DESwl__3_dz,
+                     "DESwl__0_m" => DESwl__0_m,
+                     "DESwl__1_m" => DESwl__1_m,
+                     "DESwl__2_m" => DESwl__2_m,
+                     "DESwl__3_m" => DESwl__3_m,
         
-                     "eBOSS__0_0_b" => eBOSS__0_0_b,
-                     "eBOSS__1_0_b" => eBOSS__1_0_b,
+                     "eBOSS__0_b" => eBOSS__0_b,
+                     "eBOSS__1_b" => eBOSS__1_b,
         
-                     "DECALS__0_0_b" => DECALS__0_0_b,
-                     "DECALS__1_0_b" => DECALS__1_0_b,
-                     "DECALS__2_0_b" => DECALS__2_0_b,
-                     "DECALS__3_0_b" => DECALS__3_0_b,
-                     "DECALS__0_0_dz" => DECALS__0_0_dz,
-                     "DECALS__1_0_dz" => DECALS__1_0_dz,
-                     "DECALS__2_0_dz" => DECALS__2_0_dz,
-                     "DECALS__3_0_dz" => DECALS__3_0_dz,
+                     "DECALS__0_b" => DECALS__0_b,
+                     "DECALS__1_b" => DECALS__1_b,
+                     "DECALS__2_b" => DECALS__2_b,
+                     "DECALS__3_b" => DECALS__3_b,
+                     "DECALS__0_dz" => DECALS__0_dz,
+                     "DECALS__1_dz" => DECALS__1_dz,
+                     "DECALS__2_dz" => DECALS__2_dz,
+                     "DECALS__3_dz" => DECALS__3_dz,
                     
-                     "KiDS1000__0_e_dz" => KiDS1000__0_e_dz,
-                     "KiDS1000__1_e_dz" => KiDS1000__1_e_dz,
-                     "KiDS1000__2_e_dz" => KiDS1000__2_e_dz,
-                     "KiDS1000__3_e_dz" => KiDS1000__3_e_dz,
-                     "KiDS1000__4_e_dz" => KiDS1000__4_e_dz,
-                     "KiDS1000__0_e_m" => KiDS1000__0_e_m,
-                     "KiDS1000__1_e_m" => KiDS1000__1_e_m,
-                     "KiDS1000__2_e_m" => KiDS1000__2_e_m,
-                     "KiDS1000__3_e_m" => KiDS1000__3_e_m,
-                     "KiDS1000__4_e_m" => KiDS1000__4_e_m)
+                     "KiDS1000__0_dz" => KiDS1000__0_dz,
+                     "KiDS1000__1_dz" => KiDS1000__1_dz,
+                     "KiDS1000__2_dz" => KiDS1000__2_dz,
+                     "KiDS1000__3_dz" => KiDS1000__3_dz,
+                     "KiDS1000__4_dz" => KiDS1000__4_dz,
+                     "KiDS1000__0_m" => KiDS1000__0_m,
+                     "KiDS1000__1_m" => KiDS1000__1_m,
+                     "KiDS1000__2_m" => KiDS1000__2_m,
+                     "KiDS1000__3_m" => KiDS1000__3_m,
+                     "KiDS1000__4_m" => KiDS1000__4_m)
 
     
     cosmology = LimberJack.Cosmology(Ωm, Ωb, h, ns, s8,
                                      tk_mode="emulator",
                                      Pk_mode="Halofit")
     
-    theory = Theory(cosmology, tracers_names, pairs,
-                    idx, files; Nuisances=nuisances)
-    data_vector ~ MvNormal(theory, cov_tot)
+    theory = Theory(cosmology, meta, files; Nuisances=nuisances)
+    data ~ MvNormal(theory ./ errs, cov)
 end;
-
 cycles = 6
-steps = 50
-iterations = 100
-TAP = 0.60
-adaptation = 100
-init_ϵ = 0.05
+iterations = 1000
 nchains = nprocs()
+
+adaptation = 250
+TAP = 0.65
+
+stats_model = model(fake_data)
+sampler = NUTS(adaptation, TAP;
+               metricT=AdvancedHMC.DenseEuclideanMetric)
+
 println("sampling settings: ")
 println("cycles ", cycles)
 println("iterations ", iterations)
 println("TAP ", TAP)
 println("adaptation ", adaptation)
-println("init_ϵ ", init_ϵ)
 println("nchains ", nchains)
 
 # Start sampling.
-folpath = "../chains"
-folname = string(data_set, "_TAP_", TAP)
+folpath = "../../chains"
+folname = string("DESY1_k1k_priors_EisHu_MH")
 folname = joinpath(folpath, folname)
 
 if isdir(folname)
     fol_files = readdir(folname)
-    println("Found existing file")
+    println("Found existing file ", folname)
     if length(fol_files) != 0
         last_chain = last([file for file in fol_files if occursin("chain", file)])
         last_n = parse(Int, last_chain[7])
         println("Restarting chain")
     else
+        println("Starting new chain")
         last_n = 0
     end
 else
@@ -176,15 +179,15 @@ end
 
 for i in (1+last_n):(cycles+last_n)
     if i == 1
-        chain = sample(model(data_vector), NUTS(adaptation, TAP; init_ϵ=init_ϵ), #HMC(init_ϵ, steps),
-                       MCMCDistributed(), iterations, nchains, progress=true; save_state=true)
+        chain = sample(stats_model, sampler, MCMCDistributed(),
+                       iterations, nchains, progress=true; save_state=true)
     else
         old_chain = read(joinpath(folname, string("chain_", i-1,".jls")), Chains)
-        chain = sample(model(data_vector), NUTS(adaptation, TAP; init_ϵ=init_ϵ), #HMC(init_ϵ, steps),
-                       MCMCDistributed(), iterations, nchains, progress=true; save_state=true,
-                       resume_from=old_chain)
-    end  
+        chain = sample(stats_model, sampler, MCMCDistributed(),
+                       iterations, nchains, progress=true; save_state=true, resume_from=old_chain)
+    end
     write(joinpath(folname, string("chain_", i,".jls")), chain)
     CSV.write(joinpath(folname, string("chain_", i,".csv")), chain)
     CSV.write(joinpath(folname, string("summary_", i,".csv")), describe(chain)[1])
 end
+
