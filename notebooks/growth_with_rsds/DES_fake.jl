73893c62fc1a32a805439c5a542a3dd61f662185
jaimerz <jaimerz011235813@gmail.com>
2023-02-13 16:01:34 -0800

    script

--- notebooks/growth_with_rsds/DES_fake.jl
+++ notebooks/growth_with_rsds/DES_fake.jl
@@ -20,7 +20,7 @@ using Distributed
     pars = [0.29292871, 0.7881468, 0.04007301, 0.7817957, 0.9117439,
             1.477675, 1.805091, 1.783029, 2.173222, 2.215141,
            -0.005197634, -0.008097385, -0.0008775838, 0.001324318, -0.004479689,
-            0.272602, -2.410019,
+            0.0, 0.0, #0.272602, -2.410019,
             0.04939203, 0.02605237, 0.0261737, -0.007550238,
            -0.01824749, 0.001053441, 0.004397685, 0.01410571]
                           #Ωm,      Ωb,       h,     ns,        s8,
@@ -61,23 +61,24 @@ end
                                   meta=meta, 
                                   files=files)
     #KiDS priors
-    Ωm ~ Uniform(0.2, 0.6)
+    Ωm ~ Uniform(0.1, 0.6)
     Ωb ~ Uniform(0.028, 0.065)
     h ~ TruncatedNormal(72, 5, 0.64, 0.82)
     s8 ~ Uniform(0.4, 1.2)
     ns ~ Uniform(0.84, 1.1)
     
-    #=
-    DESgc__0_b ~ Uniform(0.8, 3.0)
-    DESgc__1_b ~ Uniform(0.8, 3.0)
-    DESgc__2_b ~ Uniform(0.8, 3.0)
-    DESgc__3_b ~ Uniform(0.8, 3.0)
-    DESgc__4_b ~ Uniform(0.8, 3.0)
+    DESgc__0_b ~ Truncated(LogNormal(0.5, 0.3), 0.0, 3.0)
+    DESgc__1_b ~ Truncated(LogNormal(0.5, 0.3), 0.0, 3.0)
+    DESgc__2_b ~ Truncated(LogNormal(0.5, 0.3), 0.0, 3.0)
+    DESgc__3_b ~ Truncated(LogNormal(0.5, 0.3), 0.0, 3.0)
+    DESgc__4_b ~ Truncated(LogNormal(0.5, 0.3), 0.0, 3.0)
     DESgc__0_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
     DESgc__1_dz ~ TruncatedNormal(0.0, 0.007, -0.2, 0.2)
     DESgc__2_dz ~ TruncatedNormal(0.0, 0.006, -0.2, 0.2)
     DESgc__3_dz ~ TruncatedNormal(0.0, 0.01, -0.2, 0.2)
     DESgc__4_dz ~ TruncatedNormal(0.0, 0.01, -0.2, 0.2)
+    A_IA ~ TruncatedNormal(0.0, 1.0, -5, 5) 
+    alpha_IA ~ TruncatedNormal(0.0, 1.0, -5, 5)
     DESwl__0_dz ~ TruncatedNormal(-0.001, 0.016, -0.2, 0.2)
     DESwl__1_dz ~ TruncatedNormal(-0.019, 0.013, -0.2, 0.2)
     DESwl__2_dz ~ TruncatedNormal(0.009, 0.011, -0.2, 0.2)
@@ -86,8 +87,6 @@ end
     DESwl__1_m ~ Normal(0.012, 0.023)
     DESwl__2_m ~ Normal(0.012, 0.023)
     DESwl__3_m ~ Normal(0.012, 0.023)
-    A_IA ~ Uniform(-5, 5) 
-    alpha_IA ~ Uniform(-5, 5)
 
     nuisances = Dict("DESgc__0_b" => DESgc__0_b,
                      "DESgc__1_b" => DESgc__1_b,
@@ -99,6 +98,8 @@ end
                      "DESgc__2_dz" => DESgc__2_dz,
                      "DESgc__3_dz" => DESgc__3_dz,
                      "DESgc__4_dz" => DESgc__4_dz,
+                     "A_IA" => A_IA,
+                     "alpha_IA" => alpha_IA,
                      "DESwl__0_dz" => DESwl__0_dz,
                      "DESwl__1_dz" => DESwl__1_dz,
                      "DESwl__2_dz" => DESwl__2_dz,
@@ -106,10 +107,7 @@ end
                      "DESwl__0_m" => DESwl__0_m,
                      "DESwl__1_m" => DESwl__1_m,
                      "DESwl__2_m" => DESwl__2_m,
-                     "DESwl__3_m" => DESwl__3_m,
-                     "A_IA" => A_IA,
-                     "alpha_IA" => alpha_IA,)
-    =#
+                     "DESwl__3_m" => DESwl__3_m)
 
     cosmology = Cosmology(Ωm, Ωb, h, ns, s8,
                           tk_mode="EisHu",
@@ -139,7 +137,7 @@ println("nchains ", nchains)
 
 # Start sampling.
 folpath = "../../chains"
-folname = string("DESY1_EisHu_fake_data_fixed_nui_TAP_", TAP)
+folname = string("DESY1_EisHu_fake_data_priors2_TAP_", TAP)
 folname = joinpath(folpath, folname)
 
 if isdir(folname)
